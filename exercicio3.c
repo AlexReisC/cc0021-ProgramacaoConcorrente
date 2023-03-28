31868f96f03ab2d31a9314ed18278245e0aac611
alexreis <alex.reis@aluno.ufca.edu.br>
2023-03-28 06:51:31 -0300

    Exercicio sobre reduction

--- exercicio3.c
+++ exercicio3.c
@@ -29,21 +29,35 @@ int main(){
     vetor = geraVetor(MAX);
 
     int i;
-    double soma;
+    double soma1, soma2;
 
-    double inicio = omp_get_wtime();
+    // INICIO DO SEQUENCIAL
+    double inicio1 = omp_get_wtime();
+    for (i = 0; i < MAX; i++){
+            soma1 = soma1 + vetor[i];
+    }
+    double media1 = soma1 / MAX;
+    double fim1 = omp_get_wtime();
+    
+    double tempo_S = fim1 - inicio1;
+    printf("Media sequencial: %f\nTempo Sequencial: %f", media1, tempo_S);
+    //FIM DO SEQUENCIAL
+
+    // INICIO DO PARARELO
+    double inicio2 = omp_get_wtime();
     #pragma omp parallel num_threads(4)
     {
-        #pragma omp for reduction(+:soma)
+        #pragma omp for reduction(+:soma2)
         for (i = 0; i < MAX; i++){
-            soma = soma + vetor[i];
+            soma2 = soma2 + vetor[i];
         }
     }
-    double media = soma / MAX;
-    double fim = omp_get_wtime();
-    double tempo = fim - inicio;
+    double media2 = soma2 / MAX;
+    double fim2 = omp_get_wtime();
+    double tempo_P = fim2 - inicio2;
     
-    printf("Tempo: %f\nMedia: %f", tempo, media);
-
+    printf("\nMedia Paralela: %f\nTempo paralelo: %f",media2, tempo_P);
+    // FIM DO PARALELO
+    printf("\nSpeedup %f", (tempo_P / tempo_S));
     return 0;
 }
